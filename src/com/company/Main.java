f20568ebe216ed9b7c5aae9a1363d98b4156a65a
sorrow33 <william.sireix@gmail.com>
2014-10-24 16:52:49 +0200

    Ouverture d'une fenêtre simple

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        JFrame jf = new JFrame("Taquin");
+        jf.setSize(800, 600);
+        jf.setVisible(true);
     }
 }
